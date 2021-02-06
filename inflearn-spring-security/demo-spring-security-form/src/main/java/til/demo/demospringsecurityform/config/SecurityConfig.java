909f5b181d5cce276b74e9ae0432f1eb8a579b5d
vljh246v <vljh246v@naver.com>
2021-02-07 01:59:38 +0900

    AuthenticationManagerBuilder 를 통한 기본 사용자 추가 설정

--- inflearn-spring-security/demo-spring-security-form/src/main/java/til/demo/demospringsecurityform/config/SecurityConfig.java
+++ inflearn-spring-security/demo-spring-security-form/src/main/java/til/demo/demospringsecurityform/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package til.demo.demospringsecurityform.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,5 +21,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
       .formLogin() // form 로그인을 사용
         .and()
       .httpBasic();
+  }
+
+  // 해당 메서드를 사용해서 원하는 임의의  유저 정보를 설정할 수 있다.
+  // {noop} : 패스워드 인코딩 방식으로, 접두사에 어떤 암호화 방식인지 표시해주어야 한다. {noop}는 암호화 하지 않았다.
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication()
+        .withUser("demo").password("{noop}123").roles("USER").and()
+        .withUser("admin").password("{noop}!@#").roles("ADMIN");
   }
 }
