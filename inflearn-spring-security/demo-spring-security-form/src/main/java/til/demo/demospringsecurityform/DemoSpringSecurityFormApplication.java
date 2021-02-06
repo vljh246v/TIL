47c8b25b53e58ca868e992cb9841f83d3daf2ea1
vljh246v <vljh246v@naver.com>
2021-02-07 01:11:50 +0900

    spring security 프로젝트 추가

---
+++ inflearn-spring-security/demo-spring-security-form/src/main/java/til/demo/demospringsecurityform/DemoSpringSecurityFormApplication.java
@@ -0,0 +1,13 @@
+package til.demo.demospringsecurityform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringSecurityFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringSecurityFormApplication.class, args);
+	}
+
+}
