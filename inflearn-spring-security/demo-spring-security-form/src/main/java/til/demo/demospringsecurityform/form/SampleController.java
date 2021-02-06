553b8a6c08a12e652a5071dfbffcf2c616cdae1f
vljh246v <vljh246v@naver.com>
2021-02-07 01:23:17 +0900

    view에 해당하는 각 path 설정

--- inflearn-spring-security/demo-spring-security-form/src/main/java/til/demo/demospringsecurityform/form/SampleController.java
+++ inflearn-spring-security/demo-spring-security-form/src/main/java/til/demo/demospringsecurityform/form/SampleController.java
@@ -1,5 +1,6 @@
 package til.demo.demospringsecurityform.form;
 
+import java.security.Principal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,5 +12,23 @@ public class SampleController {
   public String index(Model model){
     model.addAttribute("message", "Hello Spring Security");
     return "index";
+  }
+
+  @GetMapping("/info")
+  public String info(Model model){
+    model.addAttribute("message", "Info");
+    return "info";
+  }
+
+  @GetMapping("/dashboard")
+  public String dashboard(Model model, Principal principal){
+    model.addAttribute("message", "Hello " + principal.getName());
+    return "dashboard";
+  }
+
+  @GetMapping("/admin")
+  public String admin(Model model, Principal principal){
+    model.addAttribute("message", "Hello Admin " + principal.getName());
+    return "admin";
   }
 }
