0de2ed3be1f5de25968854e81ee25c5d2593a5cd
vljh246v <vljh246v@naver.com>
2021-02-12 15:06:51 +0900

    ThreadLocal 예제 코드 작성

--- inflearn-spring-security/demo-spring-security-form/src/main/java/til/demo/demospringsecurityform/form/SampleController.java
+++ inflearn-spring-security/demo-spring-security-form/src/main/java/til/demo/demospringsecurityform/form/SampleController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import til.demo.demospringsecurityform.account.AccountContext;
+import til.demo.demospringsecurityform.account.AccountRepository;
 
 @Controller
 public class SampleController {
@@ -13,6 +15,9 @@ public class SampleController {
 
   @Autowired
   SampleService sampleService;
+
+  @Autowired
+  AccountRepository  accountRepository;
 
   @GetMapping("/")
   public String index(Model model, Principal principal){
@@ -33,6 +38,7 @@ public class SampleController {
   @GetMapping("/dashboard")
   public String dashboard(Model model, Principal principal){
     model.addAttribute("message", "Hello " + principal.getName());
+    AccountContext.setAccount(accountRepository.findByUsername(principal.getName()));
     sampleService.dashboard();
     return "dashboard";
   }
