bb10d41072fe51a2114ee37e973b6cbc305276d7
vljh246v <vljh246v@naver.com>
2021-02-12 16:19:40 +0900

    SecurityContextHolder 에서 Authentication 개체를 불러오는 형태로 변경

--- inflearn-spring-security/demo-spring-security-form/src/main/java/til/demo/demospringsecurityform/form/SampleService.java
+++ inflearn-spring-security/demo-spring-security-form/src/main/java/til/demo/demospringsecurityform/form/SampleService.java
@@ -1,5 +1,8 @@
 package til.demo.demospringsecurityform.form;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import til.demo.demospringsecurityform.account.Account;
 import til.demo.demospringsecurityform.account.AccountContext;
@@ -8,8 +11,10 @@ import til.demo.demospringsecurityform.account.AccountContext;
 public class SampleService {
 
   public void dashboard() {
-    Account account = AccountContext.getAccount();
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    UserDetails userDetails = (UserDetails) authentication.getPrincipal();
     System.out.println("-------------------");
-    System.out.println(account.getUsername());
+    System.out.println(authentication);
+    System.out.println(userDetails.getUsername());
   }
 }
