62c72a0e606809c58cbf94d9e0b3cf4d42a84ec4
vljh246v <vljh246v@naver.com>
2021-02-16 22:01:04 +0900

    Hierarchy 사용자 설정을 통한 AccessDecisionManager 구현

--- inflearn-spring-security/demo-spring-security-form/src/main/java/til/demo/demospringsecurityform/config/SecurityConfig.java
+++ inflearn-spring-security/demo-spring-security-form/src/main/java/til/demo/demospringsecurityform/config/SecurityConfig.java
@@ -1,16 +1,48 @@
 package til.demo.demospringsecurityform.config;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.access.vote.AffirmativeBased;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
+import org.springframework.security.web.access.expression.WebExpressionVoter;
 
 @Configuration
 @Order(Ordered.LOWEST_PRECEDENCE - 100)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  public AccessDecisionManager accessDecisionManager(){
+
+    // Hierarchy 설정을 통해 ROLE_ADMIN이 ROLE_USER보다 상위라고 명시
+    RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+    roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+
+    // 설정한 Hierarchy를 WebSecurityExpressionHandler에 설정
+    DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
+    handler.setRoleHierarchy(roleHierarchy);
+
+    // 설정한 handler를 voter에 설정
+    WebExpressionVoter webExpressionVoter = new WebExpressionVoter();
+    webExpressionVoter.setExpressionHandler(handler);
+
+    // voter 리스트 생성
+    List<AccessDecisionVoter<? extends Object>> voters = Arrays.asList(webExpressionVoter);
+
+    // voter 리스트를 AccessDecisionManager에 셋팅
+    return new AffirmativeBased(voters);
+
+  }
 
   // 특정 url 기반 접근 설정을 하는 영역
   @Override
@@ -20,6 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .mvcMatchers("/admin").hasRole("ADMIN")
         .mvcMatchers("/user").hasRole("USER")
         .anyRequest().authenticated()
+        .accessDecisionManager(accessDecisionManager()) // 직접 설정한 accessDecisionManager를 setting
         .and()
       .formLogin() // form 로그인을 사용
         .and()
