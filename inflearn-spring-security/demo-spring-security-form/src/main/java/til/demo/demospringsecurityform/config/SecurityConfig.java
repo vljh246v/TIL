44a7353d9cf6ad00fbfacb5308bc57f5d37a5d1c
vljh246v <vljh246v@naver.com>
2021-02-23 23:23:32 +0900

    ignoring 사용을 위한 configure(WebSecurity web) Override

--- inflearn-spring-security/demo-spring-security-form/src/main/java/til/demo/demospringsecurityform/config/SecurityConfig.java
+++ inflearn-spring-security/demo-spring-security-form/src/main/java/til/demo/demospringsecurityform/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.access.vote.AffirmativeBased;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
@@ -82,4 +83,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   //        .withUser("demo").password("{noop}123").roles("USER").and()
   //        .withUser("admin").password("{noop}!@#").roles("ADMIN");
   //  }
+
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring().mvcMatchers("/favicon.ico");
+  }
 }
