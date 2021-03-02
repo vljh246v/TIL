095301a85815194f28acc0afd017537a106f6cdd
vljh246v <vljh246v@naver.com>
2021-03-02 21:51:04 +0900

    react 기초 앱 작성

---
+++ React/inflearn/hello-world/simple1.js
@@ -0,0 +1,22 @@
+function LikeButton() {
+    const [liked, setLiked] = React.useState(false);
+    const text = liked ? '좋아요 취소' : '좋아요';
+    return React.createElement(
+        'button',
+        { onClick: () => setLiked(!liked)},
+        text,
+    );
+}
+
+const domContainer1 = document.getElementById('root1')
+ReactDOM.render(
+    React.createElement(
+        'div',
+        null,
+        React.createElement(LikeButton),
+        React.createElement(LikeButton),
+        React.createElement(LikeButton),
+    ),
+    domContainer1,
+);
+    
