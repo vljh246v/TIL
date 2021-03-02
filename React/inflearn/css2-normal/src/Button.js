d799b67c7c033b6d2b89ede2e21cc37f7c2dddc6
vljh246v <vljh246v@naver.com>
2021-03-03 00:29:47 +0900

    classname 예제 추가

--- React/inflearn/css2-normal/src/Button.js
+++ React/inflearn/css2-normal/src/Button.js
@@ -3,9 +3,14 @@ import Style from './Button.module.css'
 import cn from 'classnames'
 
 export default function Button({size}) {
-    if(size === 'big'){
-        return <div className={cn(Style.button, Style.big)}>큰 버튼</div>
-    } else {
-        return <div className={cn(Style.button, Style.small)}>작은 버튼</div>
-    }
+    const isBig = size === 'big';
+    return <button className={cn(Style.button, {
+        [Style.big]: isBig,
+        [Style.small]: !isBig
+    })}>{isBig ? '큰 버튼' : '작은 버튼'}</button>
+    // if(size === 'big'){
+    //     return <div className={cn(Style.button, Style.big)}>큰 버튼</div>
+    // } else {
+    //     return <div className={cn(Style.button, Style.small)}>작은 버튼</div>
+    // }
 } 
