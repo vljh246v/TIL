a904be060c045f53f855912c3b6bf18a2a4ad66b
vljh246v <vljh246v@naver.com>
2021-03-02 22:01:33 +0900

    증가 감소 버튼 추가

--- React/inflearn/hello-world/simple1.js
+++ React/inflearn/hello-world/simple1.js
@@ -8,15 +8,30 @@ function LikeButton() {
     );
 }
 
-const domContainer1 = document.getElementById('root1')
-ReactDOM.render(
-    React.createElement(
+function Container() {
+    const [count, setCount] = React.useState(0);
+    return React.createElement(
         'div',
         null,
         React.createElement(LikeButton),
-        React.createElement(LikeButton),
-        React.createElement(LikeButton),
-    ),
-    domContainer1,
-);
-    
+        React.createElement(
+            'div',
+            {style: {marginTop: 20} },
+            React.createElement('span', null, '현재 카운트 : '),
+            React.createElement('span', {style: {marginRight: 10}}, count),
+            React.createElement(
+                'button',
+                {onClick: () => setCount(count + 1)},
+                '증가'
+            ),
+            React.createElement(
+                'button',
+                {onClick: () => setCount(count - 1)},
+                '감소'
+            ),
+        ),
+    );
+}
+
+const domContainer1 = document.getElementById('root1')
+ReactDOM.render(React.createElement(Container),domContainer1);
