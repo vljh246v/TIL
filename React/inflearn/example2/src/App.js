7d52d58874e1fa199dbd9f718dac5fe6c74edda7
vljh246v <vljh246v@naver.com>
2021-03-10 00:50:03 +0900

    각 컴포넌트 상태값 관리 확인

--- React/inflearn/example2/src/App.js
+++ React/inflearn/example2/src/App.js
@@ -1,19 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Counter from './Counter';
 
 export default function App(){
-  const [color, setColor] = useState('red');
-  function onClick() {
-    setColor('blue');
-  }
   return (
     <div>
       <Counter />
-      <button style={{
-        backgroundColor: color
-      }} onClick={onClick}>
-        좋아요
-      </button>
+      <Counter />
     </div>
   )
 }
