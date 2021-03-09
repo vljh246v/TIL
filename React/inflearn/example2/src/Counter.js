9706b8222b6a4f12ef80edaeba9741814daf8ef5
vljh246v <vljh246v@naver.com>
2021-03-10 00:53:07 +0900

    상태 불변변수 설정 예제 - 1

--- React/inflearn/example2/src/Counter.js
+++ React/inflearn/example2/src/Counter.js
@@ -2,21 +2,16 @@ import React, {useState} from 'react';
 import Title from './Title';
 
 export default function Counter(){
-    const [count, setCount] = useState(0);
-    const [count2, setCount2] = useState(0);
+    const [count, setCount] = useState({value:0});
     function onClick() {
-        setCount(count + 1);
-    }
-
-    function onClick2() {
-        setCount2(count2 + 1);
+        count.value += 1;
+        setCount(count);
     }
 
     return (
         <div>
-            <Title title={`현재 카운트 : ${count}`} />
+            <Title title={`현재 카운트 : ${count.value}`} />
             <button onClick={onClick}>증가</button>
-            <button onClick={onClick2}>증가2</button>
         </div>
     );
 }
