0fb8cbfcddd2dc33d2e6a8de4f8f9941b3ea00b0
vljh246v <vljh246v@naver.com>
2021-03-10 00:18:08 +0900

    react app example 추가

---
+++ React/inflearn/example1/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState} from 'react';
+
+export default function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [currentId, setCurrentId] = useState(1);
+  const [desc, setDesc] = useState('');
+  function onAdd() {
+    const todo = {id: currentId, desc: desc};
+    setCurrentId(currentId + 1);
+    setTodoList([...todoList, todo])
+  }
+  function onDelete(e){
+    const id = Number(e.target.dataset.id);
+    const newTodoList = todoList.filter(todo => todo.id !== id)
+    setTodoList(newTodoList);
+  }
+  function onSaveToServer(){}
+
+  return (
+    <div>
+      <h3>할 일 목록</h3>
+      <ul>
+        {todoList.map(todo => (
+          <li key={todo.id}>
+            <span>{todo.desc}</span>
+            <button data-id={todo.id} onClick={onDelete}>
+              삭제
+            </button>
+          </li>
+        ))}
+      </ul>
+      <input type="text" value={desc} onChange={e => setDesc(e.target.value)} />
+      <button onClick={onAdd}>추가</button>
+      <button onClick={onSaveToServer}>서버에 저장</button>
+    </div>
+  );
+}
