f259d0abb3e758eb8d68fbd0d11d9841a6a7cbc6
vljh246v <vljh246v@naver.com>
2021-03-10 00:26:39 +0900

    홀수만 표현하는 기능 추가

--- React/inflearn/example1/src/App.js
+++ React/inflearn/example1/src/App.js
@@ -4,6 +4,7 @@ export default function App() {
   const [todoList, setTodoList] = useState([]);
   const [currentId, setCurrentId] = useState(1);
   const [desc, setDesc] = useState('');
+  const [showOdd, setShowOdd] = useState(false);
   function onAdd() {
     const todo = {id: currentId, desc: desc};
     setCurrentId(currentId + 1);
@@ -20,7 +21,9 @@ export default function App() {
     <div>
       <h3>할 일 목록</h3>
       <ul>
-        {todoList.map(todo => (
+        {todoList
+        .filter((_, index) => (showOdd ? index % 2 === 0 : true))
+        .map(todo => (
           <li key={todo.id}>
             <span>{todo.desc}</span>
             <button data-id={todo.id} onClick={onDelete}>
@@ -31,6 +34,7 @@ export default function App() {
       </ul>
       <input type="text" value={desc} onChange={e => setDesc(e.target.value)} />
       <button onClick={onAdd}>추가</button>
+      <button onClick={() => {setShowOdd(!showOdd)}}>홀수 아이템만 보기</button>
       <button onClick={onSaveToServer}>서버에 저장</button>
     </div>
   );
