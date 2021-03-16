05b928a411fef8581aa86dc39a5faa73d1dff228
vljh246v <vljh246v@naver.com>
2021-03-16 22:11:57 +0900

    nstable_batchedUpdates 예제

--- React/inflearn/live/src/App.js
+++ React/inflearn/live/src/App.js
@@ -1,14 +1,20 @@
 import React, { useState, useEffect} from 'react'
+import ReactDOM from 'react-dom';
 
 export default function App(){
     const [count, setCount] = useState(0);
     function onClick() {
+      ReactDOM.unstable_batchedUpdates(() => {
         setCount(v => v + 1);
         setCount(v => v + 1);
+      });
     }
     useEffect(() => {
       window.addEventListener('click', onClick);
-      return () => window.removeEventListener('click', onClick);
+      return () => {
+        console.log('!!')
+        window.removeEventListener('click', onClick)
+      };
     })
     console.log('render called');
     return (
