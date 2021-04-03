77e32e39f9815b65f941b7fbd3f9a330c85b49f4
vljh246v <vljh246v@naver.com>
2021-04-03 18:37:05 +0900

    modify typo

--- Spring/TEST/demo/src/test/java/com/demo/teststudy/demo/mapper/TestMapperTest.java
+++ Spring/TEST/demo/src/test/java/com/demo/teststudy/demo/mapper/TestMapperTest.java
@@ -1,13 +1,11 @@
 package com.demo.teststudy.demo.mapper;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
