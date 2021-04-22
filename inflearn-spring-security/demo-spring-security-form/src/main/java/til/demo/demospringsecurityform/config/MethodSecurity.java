c38414f10be6e5c75c1cd78aaf807d154dbbdb52
vljh246v <vljh246v@naver.com>
2021-04-23 00:14:04 +0900

    메소드 시큐리티 예제 소스코드 작성

--- inflearn-spring-security/demo-spring-security-form/src/main/java/til/demo/demospringsecurityform/config/MethodSecurity.java
+++ inflearn-spring-security/demo-spring-security-form/src/main/java/til/demo/demospringsecurityform/config/MethodSecurity.java
@@ -1,2 +1,10 @@
-package til.demo.demospringsecurityform.config;public class MethodSecurity {
+package til.demo.demospringsecurityform.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
+public class MethodSecurity {
+
 }
