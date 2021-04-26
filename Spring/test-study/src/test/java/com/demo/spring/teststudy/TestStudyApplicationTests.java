96eb60d1e37c92bdeef3c74e250f4f69fb4b5b20
vljh246v <vljh246v@naver.com>
2021-04-26 23:27:44 +0900

    spring test junit 5 학습 최초 커밋

---
+++ Spring/test-study/src/test/java/com/demo/spring/teststudy/TestStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.spring.teststudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestStudyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
