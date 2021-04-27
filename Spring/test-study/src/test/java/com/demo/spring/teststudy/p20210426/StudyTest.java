af7544063fd2ff9255c6e0969645d5e9873d1e25
vljh246v <vljh246v@naver.com>
2021-04-27 22:48:16 +0900

    junit5 테스트 메소드 이름 변경 어노테이션 예제 작성

--- Spring/test-study/src/test/java/com/demo/spring/teststudy/p20210426/StudyTest.java
+++ Spring/test-study/src/test/java/com/demo/spring/teststudy/p20210426/StudyTest.java
@@ -6,16 +6,22 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
 class StudyTest {
 
   @Test
+  @DisplayName("테스트~")
   void create() {
     System.out.println("create");
     Study study = new Study();
     assertNotNull(study);
   }
+
+
 
   @Test
   void create1(){
