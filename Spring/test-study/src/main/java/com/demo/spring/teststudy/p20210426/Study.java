d3665102dce97b9b92f5ea8a65eb3a5c1e32a53c
vljh246v <vljh246v@naver.com>
2021-04-28 21:50:35 +0900

    assertThrows 예제 작성

--- Spring/test-study/src/main/java/com/demo/spring/teststudy/p20210426/Study.java
+++ Spring/test-study/src/main/java/com/demo/spring/teststudy/p20210426/Study.java
@@ -7,6 +7,9 @@ public class Study {
   private int limit;
 
   public Study(int limit) {
+    if(limit < 0){
+      throw new IllegalArgumentException("limit은 0보다 커야한다.");
+    }
     this.limit = limit;
   }
 
--- Spring/test-study/src/test/java/com/demo/spring/teststudy/p20210426/StudyTest.java
+++ Spring/test-study/src/test/java/com/demo/spring/teststudy/p20210426/StudyTest.java
@@ -14,14 +14,11 @@ class StudyTest {
   @Test
   @DisplayName("스터디 만들기 (/ω＼)")
   void create_new_study(){
-    Study study = new Study(-10);
+    IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+        () -> new Study(-10));
+    String message = illegalArgumentException.getMessage();
 
-
-    assertAll(
-        () -> assertNotNull(study),
-        () -> assertEquals(StudyStatus.DRAFT, study.getStatus(), "스터디를 처음 만들면 상태값이 DRAFT여야 한다."),
-        () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0 보다 커야 한다.")
-    );
+    assertEquals("limit은 0보다 커야한다.", message);
   }
 
   @Test
