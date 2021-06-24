2930cc4cf48b8364cc823b8bb9dce4759b88aada
vljh246v <vljh246v@naver.com>
2021-06-24 23:45:03 +0900

    kotlin study first commit!

---
+++ Kotlin/study/src/test/kotlin/com/jaehyun/kotlin/study/study/StudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jaehyun.kotlin.study.study
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
