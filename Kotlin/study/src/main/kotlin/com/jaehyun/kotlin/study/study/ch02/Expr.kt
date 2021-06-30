01475dd629427ae11b59f78edd69997b2443d297
vljh246v <vljh246v@naver.com>
2021-06-30 23:32:13 +0900

    식을 이용한 if 문 modify

--- Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch02/Expr.kt
+++ Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch02/Expr.kt
@@ -5,18 +5,16 @@ interface Expr
 class Num(val value: Int): Expr
 class Sum(val left: Expr, val right: Expr): Expr
 
-fun eval(e: Expr): Int {
-    if(e is Num) {
-        // val n = e as Num
-        return e.value
+fun eval(e: Expr): Int =
+    if (e is Num) {
+        e.value
+    } else if(e is Sum) {
+        eval(e.right) + eval(e.left)
+    } else {
+        throw IllegalArgumentException("Unknow expression")
     }
 
-    if(e is Sum) {
-        return eval(e.right) + eval(e.left)
-    }
 
-    throw IllegalArgumentException("Unknow expression")
-}
 
 fun main(args: Array<String>) {
     println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
