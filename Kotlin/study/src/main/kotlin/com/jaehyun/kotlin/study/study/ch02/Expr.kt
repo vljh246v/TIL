621eb192cb0396c7e4724b5eb14bf076e9152cf8
vljh246v <vljh246v@naver.com>
2021-06-30 23:33:59 +0900

    스마트 캐스트 / 간단한 리팩토링 예제 작성

--- Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch02/Expr.kt
+++ Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch02/Expr.kt
@@ -6,12 +6,10 @@ class Num(val value: Int): Expr
 class Sum(val left: Expr, val right: Expr): Expr
 
 fun eval(e: Expr): Int =
-    if (e is Num) {
-        e.value
-    } else if(e is Sum) {
-        eval(e.right) + eval(e.left)
-    } else {
-        throw IllegalArgumentException("Unknow expression")
+    when(e){
+        is Num -> e.value
+        is Sum -> eval(e.right) + eval(e.left)
+        else -> throw IllegalArgumentException("Unknow expression")
     }
 
 
