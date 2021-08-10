2e13b2fc6c9a44944261a84df49d111519019168
vljh246v <vljh246v@naver.com>
2021-08-10 21:37:23 +0900

    로컬 함수로 중복 제거

--- Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch03/3-6.kt
+++ Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch03/3-6.kt
@@ -3,17 +3,17 @@ package com.jaehyun.kotlin.study.study.ch03
 class User(val id: Int, val name: String, val address: String)
 
 fun saveUser(user: User){
-    if(user.name.isEmpty()){
-        throw IllegalArgumentException(
-            "Can't save user ${user.id}: empty Name"
-        )
+
+    fun validate(user: User, value: String, fieldName: String) {
+        if(value.isEmpty()){
+            throw IllegalArgumentException(
+                    "Can't save user ${user.id}: empty $fieldName"
+                    )
+        }
     }
 
-    if(user.address.isEmpty()){
-        throw IllegalArgumentException(
-            "Can't save user ${user.id}: empty Address"
-        )
-    }
+    validate(user, user.name, "Name")
+    validate(user, user.address, "Address")
 }
 
 fun main(){
