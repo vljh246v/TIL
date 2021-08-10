a7751b7119ef4e6181fca9c8df6bf951f478cd52
vljh246v <vljh246v@naver.com>
2021-08-10 21:39:50 +0900

    검증 로직을 확장 함수로 수정

--- Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch03/3-6.kt
+++ Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch03/3-6.kt
@@ -2,18 +2,22 @@ package com.jaehyun.kotlin.study.study.ch03
 
 class User(val id: Int, val name: String, val address: String)
 
-fun saveUser(user: User){
+fun User.validateVBeforeSave(){
 
     fun validate(value: String, fieldName: String) {
         if(value.isEmpty()){
             throw IllegalArgumentException(
-                    "Can't save user ${user.id}: empty $fieldName"
+                    "Can't save user ${id}: empty $fieldName"
                     )
         }
     }
 
-    validate(user.name, "Name")
-    validate(user.address, "Address")
+    validate(name, "Name")
+    validate(address, "Address")
+}
+
+fun saveUser(user: User) {
+    user.validateVBeforeSave()
 }
 
 fun main(){
