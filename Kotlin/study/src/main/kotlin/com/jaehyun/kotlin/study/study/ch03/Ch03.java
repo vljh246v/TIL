7b3bf392bda001239d20f95a3db52a6e72ea3d6d
vljh246v <vljh246v@naver.com>
2021-08-10 21:07:51 +0900

    확장함수 java 사용

---
+++ Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch03/Ch03.java
@@ -0,0 +1,9 @@
+package com.jaehyun.kotlin.study.study.ch03;
+
+public class Ch03 {
+
+    public static void main(String[] args) {
+        char kotlin = _3_3Kt.lastChar("Kotlin");
+        System.out.println(kotlin);
+    }
+}
