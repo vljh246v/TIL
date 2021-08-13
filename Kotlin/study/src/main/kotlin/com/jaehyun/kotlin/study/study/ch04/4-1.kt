f52f171ff8e9106bca7b10d7a5fc2da4f1a04327
vljh246v <vljh246v@naver.com>
2021-08-14 01:06:11 +0900

    default 메소드 충돌 제거 및 super 상위 타입 지정

--- Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch04/4-1.kt
+++ Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch04/4-1.kt
@@ -5,9 +5,21 @@ interface Clickable {
     fun showOff() = println("I'm clickable!")
 }
 
-class Button : Clickable {
+interface Focusable {
+    fun setFocus(b: Boolean) =
+        println("I ${if(b) "got" else "lost"} focus.")
+
+    fun showOff() = println("I'm focusable!!")
+}
+
+class Button : Clickable, Focusable {
     override fun click() {
         println("I was clicked")
+    }
+
+    override fun showOff() {
+        super<Clickable>.showOff()
+        super<Focusable>.showOff()
     }
 }
 
