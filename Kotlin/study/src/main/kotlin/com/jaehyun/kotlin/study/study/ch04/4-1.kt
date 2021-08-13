771ffc22a7e50160a6b9fdbd4956565cb063c601
vljh246v <vljh246v@naver.com>
2021-08-14 01:16:50 +0900

    open 및 final 기본 예제 작성

--- Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch04/4-1.kt
+++ Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch04/4-1.kt
@@ -7,7 +7,7 @@ interface Clickable {
 
 interface Focusable {
     fun setFocus(b: Boolean) =
-        println("I ${if(b) "got" else "lost"} focus.")
+        println("I ${if (b) "got" else "lost"} focus.")
 
     fun showOff() = println("I'm focusable!!")
 }
@@ -23,6 +23,22 @@ class Button : Clickable, Focusable {
     }
 }
 
-fun main(){
+open class RichButton : Clickable { //  열려있는 class
+    fun disable() {} // 하위 클래스에서 오버라이드 불가
+    open fun animate() {} // 하위 클래스에서 오버라이드 가능
+    override fun click() { // 오버라이드한 메소드는 기본적으로 open
+        println("Not yet implemented")
+    }
+}
+
+open class RichButton2 : Clickable {
+    final override fun click() { // final 을 붙여 오버라이드 불가로 변경
+        println("Not yet implemented")
+    }
+
+}
+
+
+fun main() {
     Button().click()
 }
