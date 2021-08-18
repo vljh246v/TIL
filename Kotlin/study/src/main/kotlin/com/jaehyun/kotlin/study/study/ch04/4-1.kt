409ce4086e8b8fc957a881c7b690b791f23899bb
vljh246v <vljh246v@naver.com>
2021-08-18 23:20:22 +0900

    기존 식 표현 예제 작성

--- Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch04/4-1.kt
+++ Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch04/4-1.kt
@@ -71,5 +71,18 @@ class Outer {
     }
 }
 
+interface Expr
+
+class Num(val value: Int) : Expr
+class Sum(val left: Expr, val right: Expr) : Expr
+
+fun eval(e: Expr): Int =
+    when(e){
+        is Num -> e.value
+        is Sum -> eval(e.right) + eval(e.left)
+        else ->
+            throw IllegalArgumentException("!")
+    }
+
 fun main() {
 }
