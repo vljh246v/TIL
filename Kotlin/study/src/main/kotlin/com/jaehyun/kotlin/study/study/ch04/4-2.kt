dbf2369c9415bd78b14ae5e44d1d666a79fb851f
vljh246v <vljh246v@naver.com>
2021-08-20 00:20:51 +0900

    부 생성자 상속 예제

--- Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch04/4-2.kt
+++ Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch04/4-2.kt
@@ -7,6 +7,8 @@ class User(val nickname: String, val isSubscribed: Boolean = true)
 class Context
 class AttributeSet
 
+val MY_STYLE = AttributeSet()
+
 open class View2 {
     constructor(ctx: Context) {
         // 코드
@@ -16,6 +18,22 @@ open class View2 {
         // 코드
     }
 }
+
+class MyButton : View2 {
+    constructor(ctx: Context) : super(ctx)
+    constructor(ctx: Context, attr: AttributeSet) : super(ctx, attr)
+}
+
+class MyButton2 : View2 {
+    constructor(ctx: Context) : this(ctx, MY_STYLE) {
+        //  코드
+    }
+
+    constructor(ctx: Context, attr: AttributeSet) : super(ctx, attr) {
+        // 코드
+    }
+}
+
 fun main() {
     val hyun = User("현석")
     println(hyun.isSubscribed)
