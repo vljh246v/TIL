bfc3c25f585c8b48a4ec9901bee87c58e5531a58
vljh246v <vljh246v@naver.com>
2021-08-26 00:22:39 +0900

    equals 오버라이드

--- Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch04/4-3.kt
+++ Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch04/4-3.kt
@@ -2,9 +2,19 @@ package com.jaehyun.kotlin.study.study.ch04
 
 class Client(val name: String, val postalCode: Int) {
     override fun toString(): String = "Client(name=$name, postalCode=$postalCode)"
+    override fun equals(other: Any?): Boolean {
+        if(other == null || other !is Client)
+            return false
+        return name == other.name && postalCode == other.postalCode
+    }
 }
 
 fun main() {
     println("main")
-    println(Client("jaehyun", 123))
+    val client1 = Client("jaehyun", 123)
+    println(client1)
+    val client2 = Client("jaehyun", 123)
+
+    println(client1 == client2)
+
 }
