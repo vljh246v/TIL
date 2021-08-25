9a084d2833f0766c1dd1fb17eb6ae7a4e66e8188
vljh246v <vljh246v@naver.com>
2021-08-26 00:25:52 +0900

    data class로 변경

--- Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch04/4-3.kt
+++ Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch04/4-3.kt
@@ -1,15 +1,6 @@
 package com.jaehyun.kotlin.study.study.ch04
 
-class Client(val name: String, val postalCode: Int) {
-    override fun toString(): String = "Client(name=$name, postalCode=$postalCode)"
-    override fun equals(other: Any?): Boolean {
-        if (other == null || other !is Client)
-            return false
-        return name == other.name && postalCode == other.postalCode
-    }
-
-    override fun hashCode(): Int = name.hashCode() * 31 + postalCode
-}
+data class Client(val name: String, val postalCode: Int)
 
 fun main() {
     println("main")
