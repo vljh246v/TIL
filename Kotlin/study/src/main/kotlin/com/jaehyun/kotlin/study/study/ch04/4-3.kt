8836e87372534f5740f8924c8955821376e38eba
vljh246v <vljh246v@naver.com>
2021-08-26 00:24:59 +0900

    hashCode 오버라이드

--- Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch04/4-3.kt
+++ Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch04/4-3.kt
@@ -3,10 +3,12 @@ package com.jaehyun.kotlin.study.study.ch04
 class Client(val name: String, val postalCode: Int) {
     override fun toString(): String = "Client(name=$name, postalCode=$postalCode)"
     override fun equals(other: Any?): Boolean {
-        if(other == null || other !is Client)
+        if (other == null || other !is Client)
             return false
         return name == other.name && postalCode == other.postalCode
     }
+
+    override fun hashCode(): Int = name.hashCode() * 31 + postalCode
 }
 
 fun main() {
@@ -16,5 +18,8 @@ fun main() {
     val client2 = Client("jaehyun", 123)
 
     println(client1 == client2)
+
+    val hashSetOf = hashSetOf(client1)
+    println(hashSetOf.contains(client2))
 
 }
