f513e19923d57a5315f44c64b0b72bb6d2a54a02
vljh246v <vljh246v@naver.com>
2021-09-02 01:32:06 +0900

    all, any, count, find 간단 예제

--- Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch05/5-2.kt
+++ Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch05/5-2.kt
@@ -1,10 +1,17 @@
 package com.jaehyun.kotlin.study.study.ch05
 
-fun main() {
-    val list = listOf(1, 2, 3, 4)
-    println(list.filter { it % 2 == 0 })
-    println(list.map { it * it })
+val canBeInClub27 = { p: Person -> p.age <= 27 }
 
-    val numbers = mapOf(0 to "zero", 1 to "one")
-    println(numbers.mapValues { it.value.uppercase() })
+fun main() {
+//    val list = listOf(1, 2, 3, 4)
+//    println(list.filter { it % 2 == 0 })
+//    println(list.map { it * it })
+//
+//    val numbers = mapOf(0 to "zero", 1 to "one")
+//    println(numbers.mapValues { it.value.uppercase() })
+    val people = listOf(Person("A", 27), Person("B", 31))
+    println(people.all(canBeInClub27))
+    println(people.any(canBeInClub27))
+    println(people.count(canBeInClub27))
+    println(people.find(canBeInClub27))
 }
