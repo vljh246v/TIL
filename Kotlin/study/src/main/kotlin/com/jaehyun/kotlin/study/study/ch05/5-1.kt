358f050840b0129aaac95786c4fda3d92f0ca5e5
vljh246v <vljh246v@naver.com>
2021-09-02 01:10:54 +0900

    람다 외부 변수 사용 예제

--- Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch05/5-1.kt
+++ Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch05/5-1.kt
@@ -16,16 +16,41 @@ fun findTheOldset(people: List<Person>) {
     println(theOldset)
 }
 
+fun printMessagesWithPrefix(messages: Collection<String>, prefix: String) {
+    messages.forEach {
+        println("$prefix $it")
+    }
+}
+
+fun printProblemCounts (responses: Collection<String>) {
+    var clientErrors = 0
+    var serverErrors = 0
+
+    responses.forEach {
+        if(it.startsWith("4")) {
+            clientErrors++
+        } else if (it.startsWith("5")) {
+            serverErrors++
+        }
+    }
+
+    println("$clientErrors client errors, $serverErrors server errors")
+}
 
 fun main() {
-    // 컬렉션 직접 검색
-    findTheOldset(listOf(Person("A", 29), Person("B", 31)))
+//    // 컬렉션 직접 검색
+//    findTheOldset(listOf(Person("A", 29), Person("B", 31)))
+//
+//    // 람다를 사용
+//    println(listOf(Person("A", 29), Person("B", 31)).maxByOrNull { it.age })
+//
+//    val sum = { x: Int, y: Int -> x + y }
+//    println(sum(1, 2))
+//
+//    run { println(12) }
+    val errors = listOf("403 Forbidden", "404 Not Found")
+    printMessagesWithPrefix(errors, "Error : ")
 
-    // 람다를 사용
-    println(listOf(Person("A", 29), Person("B", 31)).maxByOrNull { it.age })
 
-    val sum = { x: Int, y: Int -> x + y }
-    println(sum(1, 2))
-
-    run { println(12) }
+    printProblemCounts(errors)
 }
