124aac22bcfa9fb75441362d984a2f0653ff1415
vljh246v <vljh246v@naver.com>
2021-09-06 22:29:31 +0900

    안전한 호출 연쇄 예제

--- Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch06/6-1.kt
+++ Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch06/6-1.kt
@@ -12,6 +12,15 @@ class Employee(val name: String, val manager: Employee?)
 
 fun managerName(employee: Employee): String? = employee.manager?.name
 
+class Address(val streetAddress: String, val zipCode: Int, val city: String, val country: String)
+class Company(val name: String, val address: Address?)
+class Person(val name: String, val company: Company?)
+
+fun Person.countryName(): String {
+    val country = this.company?.address?.country
+    return if(country != null) country else "Unknown"
+}
+
 fun main() {
 //    val x: String? = null
 //    println(strLenSafe(x))
@@ -19,9 +28,13 @@ fun main() {
 //    printAllCaps("abc")
 //    printAllCaps(null)
 
-    val ceo = Employee("boss", null)
-    val developer = Employee("dev", ceo)
+//    val ceo = Employee("boss", null)
+//    val developer = Employee("dev", ceo)
+//
+//    println(managerName(developer))
+//    println(managerName(ceo))
 
-    println(managerName(developer))
-    println(managerName(ceo))
+    val person = Person("Dmitry", null)
+    println(person.countryName())
+
 }
