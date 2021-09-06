d953c878885712240ef10b0ea1607a3fe80c4004
vljh246v <vljh246v@naver.com>
2021-09-06 22:21:51 +0900

    메소드/프로퍼티 호출에서 안전한 호출 사용 예제

--- Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch06/6-1.kt
+++ Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch06/6-1.kt
@@ -3,7 +3,25 @@ package com.jaehyun.kotlin.study.study.ch06
 fun strLenSafe (s: String?) : Int =
     if(s != null) s.length else 0
 
+fun printAllCaps (s: String?) {
+    val allCaps: String? = s?.uppercase()
+    println(allCaps)
+}
+
+class Employee(val name: String, val manager: Employee?)
+
+fun managerName(employee: Employee): String? = employee.manager?.name
+
 fun main() {
-    val x: String? = null
-    println(strLenSafe(x))
+//    val x: String? = null
+//    println(strLenSafe(x))
+//
+//    printAllCaps("abc")
+//    printAllCaps(null)
+
+    val ceo = Employee("boss", null)
+    val developer = Employee("dev", ceo)
+
+    println(managerName(developer))
+    println(managerName(ceo))
 }
