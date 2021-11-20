2bbe45e2ecbddb03299a222368c5de78cfbd5589
vljh246v <vljh246v@naver.com>
2021-11-20 18:49:17 +0900

    object book example
    오브젝트 책 읽기

---
+++ JAVA/object/src/main/java/com/demoversion/object/ObjectApplication.java
@@ -0,0 +1,13 @@
+package com.demoversion.object;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectApplication.class, args);
+	}
+
+}
