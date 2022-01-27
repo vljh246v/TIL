c5dbd91be7c028b0b5c7478247cea9768f5dc5e7
demo.lim <demo.lim@linecorp.com>
2022-01-28 01:37:43 +0900

    init

---
+++ etc/unit-test/src/main/java/com/demo/unittest/UnitTestApplication.java
@@ -0,0 +1,13 @@
+package com.demo.unittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnitTestApplication.class, args);
+    }
+
+}
---
+++ etc/unit-test/src/test/java/com/demo/unittest/UnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.unittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
