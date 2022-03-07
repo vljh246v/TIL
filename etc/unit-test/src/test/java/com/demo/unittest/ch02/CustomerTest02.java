8c6aeed6c6416d588c9ae53edebd305f240d366c
demo.lim <demo.lim@linecorp.com>
2022-03-07 11:15:17 +0900

    milestone10  학습 완료

---
+++ etc/unit-test/src/test/java/com/demo/unittest/ch02/CustomerTest02.java
@@ -0,0 +1,73 @@
+package com.demo.unittest.ch01;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+
+class CustomerTest {
+
+    @Test
+    void purchaseSucceedsWhenEnoughInventory_detroit() {
+
+        // given
+        Store store = new Store();
+        store.addInventory(Product.Shampoo, 10);
+        Customer customer = new Customer();
+
+        // when
+        boolean purchase = customer.purchase(store, Product.Shampoo, 5);
+
+        assertThat(purchase).isTrue();
+        assertThat(store.getInventory(Product.Shampoo)).isEqualTo(5);
+    }
+
+    @Test
+    void purchaseSucceedsWhenEnoughInventory_mockist() {
+
+        // given
+        Store storeMock = mock(Store.class);
+        when(storeMock.hasEnoughInventory(Product.Shampoo, 5)).thenReturn(true);
+        Customer customer = new Customer();
+
+        // when
+        boolean purchase = customer.purchase(storeMock, Product.Shampoo, 5);
+
+        assertThat(purchase).isTrue();
+        verify(storeMock, times(1)).removedInventory(Product.Shampoo, 5);
+    }
+
+    @Test
+    void purchaseFailsWhenNotEnoughInventory_detroit() {
+
+        // given
+        Store store = new Store();
+        store.addInventory(Product.Shampoo, 10);
+        Customer customer = new Customer();
+
+        // when
+        boolean purchase = customer.purchase(store, Product.Shampoo, 15);
+
+        assertThat(purchase).isFalse();
+        assertThat(store.getInventory(Product.Shampoo)).isEqualTo(10);
+    }
+
+    @Test
+    void purchaseFailsWhenNotEnoughInventory_mockist() {
+
+        // given
+        Store storeMock = mock(Store.class);
+        when(storeMock.hasEnoughInventory(Product.Shampoo, 5)).thenReturn(false);
+        Customer customer = new Customer();
+
+        // when
+        boolean purchase = customer.purchase(storeMock, Product.Shampoo, 5);
+
+        assertThat(purchase).isFalse();
+        verify(storeMock, never()).removedInventory(Product.Shampoo, 5);
+    }
+}
