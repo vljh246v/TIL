875e0cc2d1def8e9d76cbca5d16995c9b07d9d36
demo.lim <demo.lim@linecorp.com>
2022-04-02 14:28:03 +0900

    create instance use to Reflection

--- JAVA/gof/src/test/java/com/example/gof/singleton/AppTest.java
+++ JAVA/gof/src/test/java/com/example/gof/singleton/AppTest.java
@@ -3,6 +3,9 @@ package com.example.gof.singleton;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 import org.junit.jupiter.api.Test;
 
 class AppTest {
@@ -15,4 +18,13 @@ class AppTest {
         assertThat(settings1).isEqualTo(settings2);
     }
 
+    @Test
+    void notEqSettingObjectUseToReflection() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Settings settings1 = Settings.getInstance();
+        Constructor<Settings> constructor = Settings.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        Settings settings2 = constructor.newInstance();
+
+        assertThat(settings1).isNotEqualTo(settings2);
+    }
 }
