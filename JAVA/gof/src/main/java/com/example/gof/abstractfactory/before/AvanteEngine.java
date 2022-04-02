1d06d184a702db6c9520742ad70c734a3736fb65
vljh246v <vljh246v@naver.com>
2022-04-02 18:18:15 +0900

    add before example

---
+++ JAVA/gof/src/main/java/com/example/gof/abstractfactory/before/AvanteEngine.java
@@ -0,0 +1,6 @@
+package com.example.gof.abstractfactory.before;
+
+import com.example.gof.abstractfactory.Engine;
+
+public class AvanteEngine implements Engine {
+}
---
+++ JAVA/gof/src/main/java/com/example/gof/abstractfactory/before/AvanteWheel.java
@@ -0,0 +1,6 @@
+package com.example.gof.abstractfactory.before;
+
+import com.example.gof.abstractfactory.Wheel;
+
+public class AvanteWheel implements Wheel {
+}
