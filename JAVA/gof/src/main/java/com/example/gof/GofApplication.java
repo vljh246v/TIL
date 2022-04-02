b2338420ad7e5ec696b2bc848b7561faba6d56ed
demo.lim <demo.lim@linecorp.com>
2022-04-02 14:04:45 +0900

    add gof project

---
+++ JAVA/gof/src/main/java/com/example/gof/GofApplication.java
@@ -0,0 +1,13 @@
+package com.example.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GofApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GofApplication.class, args);
+    }
+
+}
---
+++ JAVA/gof/src/test/java/com/example/gof/GofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GofApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
