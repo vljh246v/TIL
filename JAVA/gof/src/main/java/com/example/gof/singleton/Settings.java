e1bbea8af9b25ff3b15673e45ee39b1f615d9b56
demo.lim <demo.lim@linecorp.com>
2022-04-02 14:10:42 +0900

    create instance use to static instance

--- JAVA/gof/src/main/java/com/example/gof/singleton/Settings.java
+++ JAVA/gof/src/main/java/com/example/gof/singleton/Settings.java
@@ -3,7 +3,13 @@ package com.example.gof.singleton;
 public class Settings {
     private Settings() {}
 
+    private static Settings instance;
+
     public static Settings getInstance() {
-        return new Settings();
+        if(instance == null) {
+            instance = new Settings();
+            return instance;
+        }
+        return instance;
     }
 }
