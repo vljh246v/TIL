cb9ac6a7ef8c5b84fad0ba5f020affbe9803b5d4
demo.lim <demo.lim@linecorp.com>
2022-04-02 14:23:38 +0900

    create instance use to static inner class

--- JAVA/gof/src/main/java/com/example/gof/singleton/Settings.java
+++ JAVA/gof/src/main/java/com/example/gof/singleton/Settings.java
@@ -1,18 +1,13 @@
 package com.example.gof.singleton;
 
 public class Settings {
+
+    private static class SettingsHolder {
+        private static final Settings INSTANCE = new Settings();
+    }
     private Settings() {}
 
-    private static volatile Settings instance;
-
     public static Settings getInstance() {
-        if(instance == null) {
-            synchronized (Settings.class) {
-                if(instance == null) {
-                    instance = new Settings();
-                }
-            }
-        }
-        return instance;
+        return SettingsHolder.INSTANCE;
     }
 }
