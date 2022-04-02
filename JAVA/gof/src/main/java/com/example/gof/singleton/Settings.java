51063d9b0a2c7fb6671d6fcbcebcc7a3189853e3
demo.lim <demo.lim@linecorp.com>
2022-04-02 14:32:18 +0900

    create instance use to Serialization & Deserialization

--- JAVA/gof/src/main/java/com/example/gof/singleton/Settings.java
+++ JAVA/gof/src/main/java/com/example/gof/singleton/Settings.java
@@ -1,6 +1,8 @@
 package com.example.gof.singleton;
 
-public class Settings {
+import java.io.Serializable;
+
+public class Settings implements Serializable {
 
     private static class SettingsHolder {
         private static final Settings INSTANCE = new Settings();
--- JAVA/gof/src/test/java/com/example/gof/singleton/AppTest.java
+++ JAVA/gof/src/test/java/com/example/gof/singleton/AppTest.java
@@ -3,6 +3,13 @@ package com.example.gof.singleton;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
@@ -24,6 +31,22 @@ class AppTest {
         Constructor<Settings> constructor = Settings.class.getDeclaredConstructor();
         constructor.setAccessible(true);
         Settings settings2 = constructor.newInstance();
+
+        assertThat(settings1).isNotEqualTo(settings2);
+    }
+
+    @Test
+    void test() throws IOException, ClassNotFoundException {
+        Settings settings1 = Settings.getInstance();
+        Settings settings2;
+
+        try (ObjectOutput out = new ObjectOutputStream(new FileOutputStream("settings.obj"))) {
+            out.writeObject(settings1);
+        }
+
+        try (ObjectInput in = new ObjectInputStream(new FileInputStream("settings.obj"))) {
+            settings2 = (Settings) in.readObject();
+        }
 
         assertThat(settings1).isNotEqualTo(settings2);
     }
