2c6d232d7c2a645850ef0fcc3a5066ff54ee5d28
vljh246v <vljh246v@naver.com>
2022-04-03 21:38:33 +0900

    add real prototype example and test code

---
+++ JAVA/gof/src/main/java/com/example/gof/prototype/java/GithubIssueData.java
@@ -0,0 +1,15 @@
+package com.example.gof.prototype.java;
+
+import lombok.Data;
+
+@Data
+public class GithubIssueData {
+
+    private int id;
+
+    private String title;
+
+    private String repositoryUser;
+
+    private String repositoryName;
+}
---
+++ JAVA/gof/src/main/java/com/example/gof/prototype/java/Student.java
@@ -0,0 +1,10 @@
+package com.example.gof.prototype.java;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class Student {
+    private final String name;
+}
