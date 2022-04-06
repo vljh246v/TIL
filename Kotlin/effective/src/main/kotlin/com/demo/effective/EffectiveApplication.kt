a594d1dd43e28815f4047d602d7e37d793110ca4
vljh246v <vljh246v@naver.com>
2022-04-07 01:29:15 +0900

    first effective kotlin commit

---
+++ Kotlin/effective/src/main/kotlin/com/demo/effective/EffectiveApplication.kt
@@ -0,0 +1,11 @@
+package com.demo.effective
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EffectiveApplication
+
+fun main(args: Array<String>) {
+    runApplication<EffectiveApplication>(*args)
+}
---
+++ Kotlin/effective/src/test/kotlin/com/demo/effective/EffectiveApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.demo.effective
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EffectiveApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
