c78568494e33aac95e80e7df67b740802bfd6745
vljh246v <vljh246v@naver.com>
2022-04-09 17:58:49 +0900

    add before decorator example code

---
+++ JAVA/gof/src/main/java/com/example/gof/structural/decorator/before/CommentService.java
@@ -0,0 +1,7 @@
+package com.example.gof.structural.decorator.before;
+
+public class CommentService {
+    public void addComment(String comment) {
+        System.out.println(comment);
+    }
+}
---
+++ JAVA/gof/src/main/java/com/example/gof/structural/decorator/before/TrimmingCommentService.java
@@ -0,0 +1,14 @@
+package com.example.gof.structural.decorator.before;
+
+public class TrimmingCommentService extends CommentService {
+
+    @Override
+    public void addComment(String comment) {
+        super.addComment(trim(comment));
+    }
+
+    private String trim(String comment) {
+        return comment.replace("...", "");
+    }
+
+}
