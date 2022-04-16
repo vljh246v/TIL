ba0fbd1e785f6bfb6fc853c352484f0bf4195ebb
vljh246v <vljh246v@naver.com>
2022-04-16 19:41:23 +0900

    add command after example code

--- JAVA/gof/src/test/java/com/example/gof/behavioral/chain/after/ClientTest.java
+++ JAVA/gof/src/test/java/com/example/gof/behavioral/chain/after/ClientTest.java
@@ -1,10 +1,11 @@
 package com.example.gof.behavioral.chain.after;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class ClientTest {
 
-    @Deprecated(since = "dev test")
+    @Disabled(value = "dev test")
     @Test
     public void test1() {
         RequestHandler chain = new AuthRequestHandler(new LoggingRequestHandler(new PrintRequestHandler(null)));
