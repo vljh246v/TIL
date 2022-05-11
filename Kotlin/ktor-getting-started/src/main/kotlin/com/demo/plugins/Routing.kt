1f18c8b9d5cb69b0580e1868918bd315501a4cca
vljh246v <vljh246v@naver.com>
2022-05-11 23:39:40 +0900

    separate sample routing code

--- Kotlin/ktor-getting-started/src/main/kotlin/com/demo/plugins/Routing.kt
+++ Kotlin/ktor-getting-started/src/main/kotlin/com/demo/plugins/Routing.kt
@@ -1,16 +1,12 @@
 package com.demo.plugins
 
+import com.demo.routes.sampleRouting
 import io.ktor.server.routing.*
-import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
 
 fun Application.configureRouting() {
 
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
+       sampleRouting()
     }
 }
