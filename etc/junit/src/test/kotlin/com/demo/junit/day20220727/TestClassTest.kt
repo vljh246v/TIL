5df3d98aff8fb2214144507d005bdbdb453b22f5
vljh246v <vljh246v@naver.com>
2022-07-30 19:11:21 +0900

    add example code for various types of dynamic test

--- etc/junit/src/test/kotlin/com/demo/junit/day20220727/TestClassTest.kt
+++ etc/junit/src/test/kotlin/com/demo/junit/day20220727/TestClassTest.kt
@@ -1,18 +1,55 @@
 package com.demo.junit.day20220727
 
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.TestFactory
-import org.junit.jupiter.api.function.Executable
+import java.util.stream.IntStream
+import java.util.stream.Stream
 
 internal class TestClassTest {
     @TestFactory
     fun dynamicTestWithCollection(): List<DynamicTest> {
         return listOf(
             DynamicTest.dynamicTest("Add test") {
-                assertEquals(4, Math.addExact(1, 1))
+                assertEquals(2, Math.addExact(1, 1))
+            },
+            DynamicTest.dynamicTest("Multiply Test") {
+                assertEquals(4, Math.addExact(2, 2))
             }
         )
+    }
+
+    @TestFactory
+    fun dynamicTestsWithIterable(): Iterable<DynamicTest> {
+        return listOf(
+            DynamicTest.dynamicTest("Add test") {
+                assertEquals(2, Math.addExact(1, 1))
+            },
+            DynamicTest.dynamicTest("Multiply Test") {
+                assertEquals(4, Math.addExact(2, 2))
+            }
+        )
+    }
+
+    @TestFactory
+    fun dynamicTestsWithIterator(): Iterator<DynamicTest> {
+        return listOf(
+            DynamicTest.dynamicTest("Add test") {
+                assertEquals(2, Math.addExact(1, 1))
+            },
+            DynamicTest.dynamicTest("Multiply Test") {
+                assertEquals(4, Math.addExact(2, 2))
+            }
+        ).iterator()
+    }
+
+    @TestFactory
+    fun dynamicTestsFromIntStream(): Stream<DynamicTest> {
+        return IntStream.iterate(0) { n -> n + 2 }
+            .limit(10)
+            .mapToObj { n ->
+                DynamicTest.dynamicTest("test $n") { assertTrue(n % 2 == 0) }
+            }
     }
 }
