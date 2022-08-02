336bf527437cc173535a0cb9bb478e88c6df71d2
vljh246v <vljh246v@naver.com>
2022-08-02 23:19:11 +0900

    Add @JsonPropertyOrder Test code

--- Kotlin/study/src/test/kotlin/com/jaehyun/kotlin/study/jackson/custom/TestClassTest.kt
+++ Kotlin/study/src/test/kotlin/com/jaehyun/kotlin/study/jackson/custom/TestClassTest.kt
@@ -2,6 +2,7 @@ package com.jaehyun.kotlin.study.jackson.custom
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter
 import com.fasterxml.jackson.annotation.JsonGetter
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -44,6 +45,21 @@ internal class TestClassTest {
         val result = ObjectMapper().writeValueAsString(bean)
         print(result)
         assertThat(result).contains("My name is")
+    }
+
+    @Test
+    fun jsonPropertyOrderTest() {
+
+        @JsonPropertyOrder(alphabetic = true)
+        class MyBean(val name: String, val age: Int, val country: String)
+
+        val bean = MyBean("My bean", 32, "KR")
+
+        val result = ObjectMapper().writeValueAsString(bean)
+        print(result)
+        assertThat(result).contains("My bean")
+        assertThat(result).contains(32.toString())
+        assertThat(result).contains("KR")
     }
 }
 
