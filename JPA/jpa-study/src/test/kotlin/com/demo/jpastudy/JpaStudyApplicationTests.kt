8b560824bb9a15d9199a9ca1decc4e9e5e04edc9
vljh246v <vljh246v@naver.com>
2022-10-03 16:16:22 +0900

    jpa test code init

---
+++ JPA/jpa-study/src/test/kotlin/com/demo/jpastudy/JpaStudyApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.demo.jpastudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JpaStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
