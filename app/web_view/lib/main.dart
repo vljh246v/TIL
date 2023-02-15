0ca5de2a4827d24e595dcf7b6dbc8c1e2f27a14d
vljh246v <vljh246v@naver.com>
2023-02-15 23:30:03 +0900

    Fix web view package version

--- app/web_view/lib/main.dart
+++ app/web_view/lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:web_view/screen/home_screen.dart';
 
 void main() {
+  
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(
     MaterialApp(
       home: HomeScreen(),
--- app/web_view/lib/screen/home_screen.dart
+++ app/web_view/lib/screen/home_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+final homeUrl = Uri.parse('https://blog.codefactory.ai');
+
 class HomeScreen extends StatelessWidget {
-  WebViewController? controller;
-  final homeUrl = 'https://blog.codefactory.ai';
+  WebViewController controller = WebViewController()
+  ..setJavaScriptMode(JavaScriptMode.unrestricted)
+  ..loadRequest(homeUrl);
 
   HomeScreen({Key? key}) : super(key: key);
 
@@ -17,20 +20,14 @@ class HomeScreen extends StatelessWidget {
           actions: [
             IconButton(
                 onPressed: () {
-                  if (controller == null) {
-                    return;
-                  }
-                  controller!.loadUrl('https://blog.codefactory.ai');
+                  controller.loadRequest(homeUrl);
                 },
                 icon: Icon(Icons.home))
           ],
         ),
-        body: WebView(
-          onWebViewCreated: (WebViewController controller) {
-            this.controller = controller;
-          },
-          initialUrl: homeUrl,
-          javascriptMode: JavascriptMode.unrestricted,
-        ));
+        body:WebViewWidget(
+          controller: controller,
+        )
+    );
   }
 }
