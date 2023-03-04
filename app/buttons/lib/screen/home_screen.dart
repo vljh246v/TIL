984e5edf4b7b91cc00b2f182876ca78e84625b65
vljh246v <vljh246v@naver.com>
2023-03-04 22:13:20 +0900

    Add flutter button

--- app/buttons/lib/screen/home_screen.dart
+++ app/buttons/lib/screen/home_screen.dart
@@ -6,9 +6,72 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-        ],
+      appBar: AppBar(
+        title: Text('버튼'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Button Style'),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.black),
+                  foregroundColor: MaterialStateProperty.resolveWith(
+                      (Set<MaterialState> state) {
+                    if (state.contains(MaterialState.pressed)) {
+                      return Colors.white;
+                    }
+                    return Colors.red;
+                  }),
+                  padding: MaterialStateProperty.resolveWith(
+                      (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return EdgeInsets.all(100.0);
+                    }
+                    return EdgeInsets.all(20.0);
+                  })),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.black,
+                shadowColor: Colors.green,
+                backgroundColor: Colors.red,
+                elevation: 10.0,
+                textStyle: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 20.0,
+                ),
+                padding: EdgeInsets.all(32.0),
+                side: BorderSide(
+                  color: Colors.black,
+                  width: 4.0,
+                ),
+              ),
+              child: Text('ElevatedButton!'),
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              child: Text('OutlinedButton'),
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.green,
+                backgroundColor: Colors.yellow,
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.brown,
+                backgroundColor: Colors.blue,
+              ),
+              child: Text('TextButton'),
+            )
+          ],
+        ),
       ),
     );
   }
