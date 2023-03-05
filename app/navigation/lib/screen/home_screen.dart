28ba7add02b627f8ae9ba76dca0341f1c4d5507b
vljh246v <vljh246v@naver.com>
2023-03-05 15:03:43 +0900

    add will pop scope

--- app/navigation/lib/screen/home_screen.dart
+++ app/navigation/lib/screen/home_screen.dart
@@ -7,41 +7,46 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MainLayout(
-      title: 'Home Screen',
-      children: [
-        ElevatedButton(
-          onPressed: () {
-            print(Navigator.of(context).canPop());
-          },
-          child: Text('Can Pop'),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: Text('Pop'),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).maybePop();
-          },
-          child: Text('Maybe Pop'),
-        ),
-        ElevatedButton(
-          onPressed: () async {
-            final result = await Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (BuildContext context) => RouteOneScreen(
-                  number: 123,
+    return WillPopScope(
+      onWillPop: () async {
+        return true;
+      },
+      child: MainLayout(
+        title: 'Home Screen',
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              print(Navigator.of(context).canPop());
+            },
+            child: Text('Can Pop'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Pop'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).maybePop();
+            },
+            child: Text('Maybe Pop'),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              final result = await Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) => RouteOneScreen(
+                    number: 123,
+                  ),
                 ),
-              ),
-            );
-            print(result);
-          },
-          child: Text('Push'),
-        )
-      ],
+              );
+              print(result);
+            },
+            child: Text('Push'),
+          )
+        ],
+      ),
     );
   }
 }
