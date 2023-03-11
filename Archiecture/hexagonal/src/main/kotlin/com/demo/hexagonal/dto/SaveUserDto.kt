3a9cbda6266d01036ba278291e6574a7f46fd100
vljh246v <vljh246v@naver.com>
2023-03-11 21:34:49 +0900

    Init old example2

--- Archiecture/hexagonal/src/main/kotlin/com/demo/hexagonal/dto/SaveUserDto.kt
+++ Archiecture/hexagonal/src/main/kotlin/com/demo/hexagonal/dto/SaveUserDto.kt
@@ -1,6 +1,11 @@
 package com.demo.hexagonal.dto
 
 data class SaveUserDto(
+    val content: User
+
+)
+
+data class User(
     val name: String,
     val age: Int,
     val birth: String, // YYYY-MM-DD
