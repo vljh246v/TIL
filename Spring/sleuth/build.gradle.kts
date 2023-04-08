6368d538748fa0d514ce8fcacbcbd3537d440a88
vljh246v <vljh246v@naver.com>
2023-04-08 15:21:23 +0900

    Fix dependencies

--- Spring/sleuth/build.gradle.kts
+++ Spring/sleuth/build.gradle.kts
@@ -1,15 +1,17 @@
+
+
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-plugins {
-	id("org.springframework.boot") version "2.7.11-SNAPSHOT"
-	id("io.spring.dependency-management") version "1.0.15.RELEASE"
-	kotlin("jvm") version "1.6.21"
-	kotlin("plugin.spring") version "1.6.21"
-}
+		plugins {
+			id("org.springframework.boot") version "2.7.11-SNAPSHOT"
+			id("io.spring.dependency-management") version "1.0.15.RELEASE"
+			kotlin("jvm") version "1.6.21"
+			kotlin("plugin.spring") version "1.6.21"
+		}
 
 group = "com.demo"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
 	mavenCentral()
@@ -17,19 +19,26 @@ repositories {
 	maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.cloud:spring-cloud-starter-sleuth")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
+extra["springCloudVersion"] = "2021.0.6"
 
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.cloud:spring-cloud-sleuth-zipkin")
+	implementation("org.springframework.cloud:spring-cloud-starter-sleuth")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
 }
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
+		jvmTarget = "11"
 	}
 }
 
