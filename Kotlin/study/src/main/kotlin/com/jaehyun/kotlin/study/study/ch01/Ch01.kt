9475aedf952a4177560e122981852c164e558b65
vljh246v <vljh246v@naver.com>
2023-05-24 20:42:37 +0900

    move dir

--- Kotlin/study/src/main/kotlin/com/jaehyun/kotlin/study/study/ch01/Ch01.kt
+++
@@ -1,9 +0,0 @@
-package com.jaehyun.kotlin.study.study.ch01
-
-fun main(args: Array<String>){
-    val persons = listOf(Person("영희"), Person("철수", age=29))
-    val oldest = persons.maxByOrNull { it.age ?: 0 }
-
-    println("나이가 가장 많은 사람: $oldest")
-
-}
--- etc/junit/src/main/kotlin/com/demo/junit/day20220725/TestClass.kt
+++
@@ -1,7 +0,0 @@
-package com.demo.junit.day20220725
-
-class TestClass {
-    fun oddCheck(number: Int): Boolean {
-        return number % 2 != 0
-    }
-}
