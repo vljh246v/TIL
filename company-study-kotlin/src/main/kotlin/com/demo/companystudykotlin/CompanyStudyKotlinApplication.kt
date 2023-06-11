e997eda88512aeac9534c22df432c439fbca484d
vljh246v <vljh246v@naver.com>
2023-06-11 20:44:51 +0900

    add coroutine study

---
+++ company-study-kotlin/src/main/kotlin/com/demo/companystudykotlin/CompanyStudyKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.demo.companystudykotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CompanyStudyKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<CompanyStudyKotlinApplication>(*args)
+}
